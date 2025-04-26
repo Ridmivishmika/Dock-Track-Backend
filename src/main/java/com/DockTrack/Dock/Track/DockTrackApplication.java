8fbc951ca4f220bbf317d29199519032c263e908
Abeysiri A.D.R.V. <vishmika2adr@gmail.com>
2025-04-26 12:15:43 +0530

    Initial commit

---
+++ src/main/java/com/DockTrack/Dock/Track/DockTrackApplication.java
@@ -0,0 +1,13 @@
+package com.DockTrack.Dock.Track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DockTrack/Dock/Track/DockTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DockTrack.Dock.Track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
